621501a1f3522ec0cf5e170865e4aff08b907490
elzbieta-f <elzbieta.fulga@gmail.com>
2022-07-14 20:11:13 +0300

    task submission

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        for (int i = 1; i <=sideLength; i++) {
+            if (i==(sideLength+1)/2) {
+                for (int j = 0; j < sideLength; j++) {
+                    System.out.print("8");
+                }
+            } else {
+                for (int j = 1; j <= sideLength; j++) {
+                    if (j == (sideLength+1)/2){
+                        System.out.print("8");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println("");
+        }
     }
 }
